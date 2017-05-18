c9eb7ad737553dea482561f18bdcf5a08d7f838f
mtotomgonjwa <mtoto.mgonjwa@gmail.com>
2017-05-18 12:28:22 +0300

    Updated Dashboard View

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,6 +16,97 @@
                     <li class="active">Dashboard</li>
                 </ol>
             </section>
+            <section class="content">
+
+                <div class="row">
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Event Traffic</span>
+                                <span class="info-box-number"> {{($dashstats['events'])}}
+                                    <small>#</small></span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Likes</span>
+                                <span class="info-box-number">41,410</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+
+                    <!-- fix for small devices only -->
+                    <div class="clearfix visible-sm-block"></div>
+
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Sales</span>
+                                <span class="info-box-number">760</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">New Members</span>
+                                <span class="info-box-number">2,000</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col --> <!-- fix for small devices only -->
+                    <div class="clearfix visible-sm-block"></div>
+
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-green"><i class="ion ion-ios-people-outline"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Groups</span>
+                                <span class="info-box-number"> {{($dashstats['groups'])}}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-yellow"><i class="ion ion-ios-contact"></i></span>
+
+                            <div class="info-box-content">
+                                <span class="info-box-text">Users</span>
+                                <span class="info-box-number"> {{($dashstats['users'])}}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                        </div>
+                        <!-- /.info-box -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+
+            </section>
 
 
             <!-- /.content -->
