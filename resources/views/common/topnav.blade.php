18d5a4f28baf415ce25b94b9089b8348910e7c9f
mtotomgonjwa <mtoto.mgonjwa@gmail.com>
2017-05-18 12:28:03 +0300

    Modified Top Nav Partial

--- resources/views/common/topnav.blade.php
+++ resources/views/common/topnav.blade.php
@@ -1,10 +1,11 @@
+
 <header class="main-header">
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="#" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>C</b>PT</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Church </b> Portal</span>
+        <span class="logo-lg"><b>Church</b> Portal</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
@@ -12,7 +13,20 @@
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
+        @if( Auth::check())
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
 
-
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#" aria-expanded="true"> <i
+                                class="fa fa-user"> {{Auth::user()->name}}</i> <b class="caret"></b></a>
+                    <!-- Dropdown menu -->
+                    <ul class="dropdown-menu">
+                        <li><a href="{{url('/logout')}}"><i class="fa fa-sign-out"></i> Logout</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        @endif
     </nav>
 </header>
