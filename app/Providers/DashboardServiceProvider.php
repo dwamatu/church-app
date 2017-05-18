150ed9b33956bda01322099515ed9781bcfc155e
mtotomgonjwa <mtoto.mgonjwa@gmail.com>
2017-05-18 11:04:04 +0300

    Added Dashboard Service Provider

---
+++ app/Providers/DashboardServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class DashboardServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer(
+            'dashboard',
+            'App\Http\ViewComposers\DashboardComposer'
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
